438adf56174f6a4a638fd90da26f2cfee32f59a0
Daniel Feles <dfeles@gmail.com>
2020-06-21 15:44:22 +0200

    proba

--- src/js/index.js
+++ src/js/index.js
@@ -7,10 +7,10 @@ var lucidImages = [];
 function preload() {
     for (var i = 0; i < 128; i++) {
       faceImages[i] = new Image();
-      faceImages[i].src = "/public/images/face/" + i + ".jpg";
+      faceImages[i].src = "public/images/face/" + i + ".jpg";
 
       lucidImages[i] = new Image();
-      lucidImages[i].src = "/public/images/lucid/" + i + ".jpg";
+      lucidImages[i].src = "public/images/lucid/" + i + ".jpg";
 
       var container = document.createElement("div");
       container.className = "mt-5 col-lg-4 col-md-6 col-sm-12"
